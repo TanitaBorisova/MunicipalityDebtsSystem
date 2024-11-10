586945db88f3dee810a49458971fa37aaf238eef
Таня Борисова <tlazarova@is-bg.net>
2024-11-10 12:24:38 +0200

    ApplicationClasses

---
+++ MunicipalityDebtsSystem/Areas/Identity/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MunicipalityDebtsSystem.Areas.Identity
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    {
+        [Required]
+        public string RoleNameBG { get; set; } = null!;
+    }
+}
---
+++ MunicipalityDebtsSystem/Areas/Identity/ApplicationUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using MunicipalityDebtsSystem.Infrastructure.Data.Constants;
+using MunicipalityDebtsSystem.Infrastructure.Data.Models.Nomenclatures;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MunicipalityDebtsSystem.Areas.Identity
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+        [Required]
+        [MaxLength(ValidationConstants.UserMaxLength)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(ValidationConstants.UserMaxLength)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        public int DataEbkId { get; set; }
+
+        [ForeignKey(nameof(DataEbkId))]
+        public EBK DataEbk { get; set; } = null!;
+    }
+}
